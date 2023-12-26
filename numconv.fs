1448c09a95953034e23f48a9de34d23c4b836a34
Bryan Zimmer <bazthelinuxguy@gmail.com>
2023-12-26 10:57:42 -0600

    This commit has timestamps added to notes
    This is the version that uses a linked list instead of an array and a queue.

--- numconv.fs
+++ numconv.fs
@@ -24,10 +24,9 @@ case
 : 0fill ( n width )
 	2dup how-many-zeros 0 u+do 0 1 .r loop drop dup testlen .r ;
 decimal
-: dispdt -rot dup testlen .r 47 emit dup testlen .r 47 emit dup testlen .r ;
-
-\ : dispdt -rot dup testlen .r 47 emit dup testlen .r 47 emit  . ;
-
+: dispdt ( DD MM YY -- )
+	-rot dup testlen .r 47 emit dup testlen .r 47 emit
+	2000 + dup testlen .r ;
 
 0 value AMPM \ 0 = "AM", 1 = "PM"
 decimal
